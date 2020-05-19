31bb44e8f8511f96b70e2fee3954c26a117595f4
liqiang <2415621370@qq.com>
2020-05-19 10:11:01 +0800

    高亮显示

--- pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
+++ pinyougou-search-service/src/main/java/com/pinyougou/search/service/impl/ItemSearchServiceImpl.java
@@ -5,11 +5,14 @@ import com.pinyougou.pojo.TbItem;
 import com.pinyougou.search.service.ItemSearchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
-import org.springframework.data.solr.core.query.Criteria;
-import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.*;
+import org.springframework.data.solr.core.query.result.HighlightEntry;
+import org.springframework.data.solr.core.query.result.HighlightPage;
 import org.springframework.data.solr.core.query.result.ScoredPage;
 
+import javax.swing.text.Highlighter;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -34,15 +37,40 @@ public class ItemSearchServiceImpl implements ItemSearchService {
     @Override
     public Map<String, Object> search(Map searchMap) {
         Map map =  new HashMap();
-        SimpleQuery query = new SimpleQuery();
-        //添加查询条件
+        //构造高亮查询对象
+        HighlightQuery query =  new SimpleHighlightQuery();
+        //创建高亮查询设置对象，设置高亮显示的域（可以设置多个，获取高亮数据是通过下标获取）
+        HighlightOptions highlightOptions =
+                new HighlightOptions().addField("item_title");
+        //高亮的前缀
+        highlightOptions.setSimplePrefix("<em style='color:red'>");
+        //高亮的后缀
+        highlightOptions.setSimplePostfix("</em>");
+        //设置高亮选项
+        query.setHighlightOptions(highlightOptions);
+        //关键词的搜索
         Criteria criteria =
                 new Criteria("item_keywords").is(searchMap.get("keywords"));
         query.addCriteria(criteria);
 
-        ScoredPage<TbItem> page = solrTemplate.queryForPage(query, TbItem.class);
+        //执行查询，注意：这里面的结果集中的getContent返回的结果集中，并没有带上高亮，需要我们自行处理
+        HighlightPage<TbItem> page = solrTemplate.queryForHighlightPage(query, TbItem.class);
+        //高亮显示数据结果集的入口
+      //  List<HighlightEntry<TbItem>> highlighted = page.getHighlighted();
 
-        map.put("rows",page.getContent());
+        //从结果集中取出数据，最先遍历高亮结果入口集
+        for(HighlightEntry<TbItem> entry:page.getHighlighted()){
+            //健壮性的判断
+            if(entry.getHighlights().size()>0 && entry.getHighlights().get(0).getSnipplets().size()>0){
+                //通过高亮域下标从高亮结果入口集中再获取高亮数据   <em style='color:red'>联想</em>
+                String title = entry.getHighlights().get(0).getSnipplets().get(0);
+                //从entry 中获取当前遍历到的item，并将高亮数据设置到标题title
+                TbItem item = entry.getEntity();
+                item.setTitle(title);
+            }
+        }
+        List<TbItem> contentList = page.getContent();
+        map.put("rows",contentList);
         return map;
     }
 }
